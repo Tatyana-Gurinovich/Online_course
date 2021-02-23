9fb423c1bf4aca2724ff68c43af9382a193f51fa
Tatyana-Gurinovich <tgurinovich2005@gmail.com>
2021-02-23 23:22:04 +0300

    свойства объекта + get set

--- Object_&_Function/Creating_and_configuring_objects/script.js
+++ Object_&_Function/Creating_and_configuring_objects/script.js
@@ -1,6 +1,12 @@
 let ford = Object.create({
     calculateDistancePerYear:function(){
-        console.log('calculate')
+       Object.defineProperty(this, 'distancePerYear',{
+           value:  this.distance / this.age,
+           enumerable:true,
+           writable:false ,
+           configurable:false
+       })//задаёт новые поля.Первое значение показывает к какому объекту добавляем поле.
+       // второе название ключа
     }
 },{
     name:{
@@ -22,7 +28,19 @@ let ford = Object.create({
         value:120500,
         enumerable:true,
         writable:true,
-        configurable:false}
+        configurable:false},
+    age:{
+        enumerable:true,
+        //get будет возвращать в тот момент, когда мы будем обращаться к полю age
+        get :function(){
+           console.log('Получает возвраст')
+           return new Date().getFullYear() - this.year
+        },
+        set: function(){
+           console.log('Устанавлеваем значение')
+        }
+
+    }
 })//Object -- глобальный класс. create его метод, который создаёт объекты
 //В create можно передавать два параметра. ПЕрвый отвечает за прототип данного объекта. Второй отвечает за свойства данного объекта
 
@@ -31,3 +49,9 @@ console.log(ford)
 //свойство writable позволяет(или нет) изменять значения в объекте
 // свойство configurable позволяет(или нет) удалять ключиы
 //свойство enumerable позволяет открыть область видимости для определённых свойств
+
+for(let key in ford ){
+    if(ford.hasOwnProperty(key)){
+    console.log(key, ford[key])
+    }
+}
