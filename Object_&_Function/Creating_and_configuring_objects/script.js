da953ceddabc55304c63297ac9ba415e71a11142
Tatyana-Gurinovich <tgurinovich2005@gmail.com>
2021-02-23 22:49:35 +0300

    Свойства enumerable, writable, configurable

--- Object_&_Function/Creating_and_configuring_objects/script.js
+++ Object_&_Function/Creating_and_configuring_objects/script.js
@@ -1,9 +1,33 @@
-let ford = Object.create({},{
-    name:{value: 'ford'},
-    model:{value:'Focus'},
-    year:{value:2015},
-    distance:{value:120500}
+let ford = Object.create({
+    calculateDistancePerYear:function(){
+        console.log('calculate')
+    }
+},{
+    name:{
+        value: 'ford',
+        enumerable:true,
+        writable:false,
+        configurable:false},
+    model:{
+        value:'Focus',
+        enumerable:true,
+        writable:false,
+        configurable:false},
+    year:{
+        value:2015,
+        enumerable:true,
+        writable:false,
+        configurable:false},
+    distance:{
+        value:120500,
+        enumerable:true,
+        writable:true,
+        configurable:false}
 })//Object -- глобальный класс. create его метод, который создаёт объекты
 //В create можно передавать два параметра. ПЕрвый отвечает за прототип данного объекта. Второй отвечает за свойства данного объекта
 
 console.log(ford)
+
+//свойство writable позволяет(или нет) изменять значения в объекте
+// свойство configurable позволяет(или нет) изменять значение ключей
+//свойство enumerable позволяет открыть область видимости для определённых свойств
