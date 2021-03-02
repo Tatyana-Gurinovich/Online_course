d57589c0b99c647c12bdab42dc1aaae83da741a6
Tatyana-Gurinovich <tgurinovich2005@gmail.com>
2021-03-02 23:17:30 +0300

    lesson 51. Получение ключей и значений из объекта

--- Object_&_Function/Object_key_interation/script.js
+++ Object_&_Function/Object_key_interation/script.js
@@ -3,3 +3,18 @@ let person={
     age:28,
     job:'Frontend'
 }
+
+//воводит значения положенные в ячейуи объекта
+for(let key in person){
+    if(person.hasOwnProperty(key))//чтобы выводились только значения данного объекта
+    console.log(person[key])
+}
+
+
+let keys =Object.keys(person)//более современный способ получения значений из объекта. Вернёт массив ключей данного объекта
+console.log(keys)
+
+//способ для вывода значений в ячейках. Object.keys не идёт по прототипу,а затрагивает только собственные ключи у объекта
+Object.keys(person).forEach(function(key){
+console.log(person[key])
+})//в forEach() мы передаём функцию, которая будет вызываться на каждой итерации массива
